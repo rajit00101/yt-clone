a1399bd787a8b874c801087e340198476c9198bb
Rajit <rajits9106@gmail.com>
2025-09-22 15:38:06 -0400

    adding relevant files

---
+++ video-processing-service/src/index.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import ffmpeg from "fluent-ffmpeg";
+
+
+const app = express();
+app.use(express.json());
+
+app.post("/process-video", (req, res) => {
+    // Get path of input video file from the request body
+    const inputFilePath = req.body.inputFilePath
+    const outputFilePath = req.body.outputFilePath
+
+    if (!inputFilePath) {
+        res.status(400).send("Bad Request: Missing input file path.")
+    }
+
+    if (!outputFilePath) {
+        res.status(400).send("Bad Request: Missing output file path.")
+    }
+
+    ffmpeg(inputFilePath)
+        .outputOptions("-vf", "scale=-1:360") //360p
+        .on("end", () => {
+            return res.status(200).send("Processing finished successfully.")
+        })
+        .on("error", (err) => {
+            console.log('An error occured: ${err.messaage}');
+            res.status(500).send('Internal Server Error: ' + err.message)
+        })
+        .save(outputFilePath);
+});
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
