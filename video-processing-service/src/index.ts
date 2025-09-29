4ec85ca35d09c38f7537718f73385b00f8c3e7ec
Rajit <rajits9106@gmail.com>
2025-09-29 17:53:03 -0400

    wip

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,35 +1,35 @@
-import express from "express";
-import ffmpeg from "fluent-ffmpeg";
-
+import express from 'express';
+import ffmpeg from 'fluent-ffmpeg';
 
 const app = express();
 app.use(express.json());
 
-app.post("/process-video", (req, res) => {
-    // Get path of input video file from the request body
-    const inputFilePath = req.body.inputFilePath
-    const outputFilePath = req.body.outputFilePath
+app.post('/process-video', (req, res) => {
 
-    if (!inputFilePath) {
-        res.status(400).send("Bad Request: Missing input file path.")
-    }
+  // Get the path of the input video file from the request body
+  const inputFilePath = req.body.inputFilePath;
+  const outputFilePath = req.body.outputFilePath;
 
-    if (!outputFilePath) {
-        res.status(400).send("Bad Request: Missing output file path.")
-    }
+  // Check if the input file path is defined
+  if (!inputFilePath || !outputFilePath) {
+    return res.status(400).send('Bad Request: Missing file path');
+  }
 
-    ffmpeg(inputFilePath)
-        .outputOptions("-vf", "scale=-1:360") //360p
-        .on("end", () => {
-            return res.status(200).send("Processing finished successfully.")
-        })
-        .on("error", (err) => {
-            console.log('An error occured: ${err.messaage}');
-            res.status(500).send('Internal Server Error: ' + err.message)
-        })
-        .save(outputFilePath);
+  // Create the ffmpeg command
+  ffmpeg(inputFilePath)
+    .outputOptions('-vf', 'scale=-1:360') // 360p
+    .on('end', function() {
+        console.log('Processing finished successfully');
+        res.status(200).send('Processing finished successfully');
+    })
+    .on('error', function(err: any) {
+        console.log('An error occurred: ' + err.message);
+        res.status(500).send('An error occurred: ' + err.message);
+    })
+    .save(outputFilePath);
 });
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+    console.log(`Server is running on port ${port}`);
 });
